228952fd079b050748ebf6cad3650d81102a6be6
Romua1d Shmidtelson <d-w-b@mail.ru>
2019-07-24 08:44:08 +0300

    Update update_profile.py

--- modules/update_profile.py
+++ modules/update_profile.py
@@ -11,22 +11,25 @@ PATH_TO_IMAGE = os.path.join(APP_PATH, 'ava.jpg')
 
 
 def run_update_profile():
-    # CHANGE PROFILE
-    fake = Faker()
-    client(UpdateProfileRequest(
-        first_name=fake.first_name_male(),
-        last_name=fake.last_name_male(),
-        about=fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None)
-    ))
-
-    # GET IMAGE
-    img_url = 'https://cataas.com/c?type=square'
-
-    with open(PATH_TO_IMAGE, 'wb') as output_file, \
-            requests.get(img_url, stream=True) as response:
-        shutil.copyfileobj(response.raw, output_file)
-
-        # CHANGE AVATAR
-        result = client(functions.photos.UploadProfilePhotoRequest(
-            file=client.upload_file(PATH_TO_IMAGE)
+    try:
+        # CHANGE PROFILE
+        fake = Faker()
+        client(UpdateProfileRequest(
+            first_name=fake.first_name_male(),
+            last_name=fake.last_name_male(),
+            about=fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None)
         ))
+
+        # GET IMAGE
+        img_url = 'https://cataas.com/c?type=square'
+
+        with open(PATH_TO_IMAGE, 'wb') as output_file, \
+                requests.get(img_url, stream=True) as response:
+            shutil.copyfileobj(response.raw, output_file)
+
+            # CHANGE AVATAR
+            result = client(functions.photos.UploadProfilePhotoRequest(
+                file=client.upload_file(PATH_TO_IMAGE)
+            ))
+    except Exception as e:
+        print(e)
