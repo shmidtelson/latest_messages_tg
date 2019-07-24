995bb0124985d4743b08e3bef2f92c43819b62f9
shmidtelson <d-w-b@mail.ru>
2019-07-24 08:42:15 +0300

    Updated

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import schedule as schedule
 from modules.latest_news_tg import get_latest_news
 from modules.update_profile import run_update_profile
 
+if __name__ == '__main__':
+    logging.basicConfig(filename="logs.log", level=logging.DEBUG)
 
-logging.basicConfig(filename="logs.log", level=logging.DEBUG)
+    get_latest_news()
+    run_update_profile()
 
-get_latest_news()
+    schedule.every().hour.do(get_latest_news)
+    schedule.every().day.at("00:30").do(run_update_profile)
 
-schedule.every().hour.do(get_latest_news)
-schedule.every().day.at("00:30").do(run_update_profile)
-
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
 
